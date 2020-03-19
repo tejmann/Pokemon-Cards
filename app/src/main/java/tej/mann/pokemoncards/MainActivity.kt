7080b3abf0d608bdcc006419b36e65dd55cbf09c
Tej partap Singh <tp3singh@uwaterloo.ca>
2020-03-19 16:56:39 -0400

    spash-screen

--- app/src/main/java/tej/mann/pokemoncards/MainActivity.kt
+++ app/src/main/java/tej/mann/pokemoncards/MainActivity.kt
@@ -10,6 +10,7 @@ import tej.mann.pokemon.PokemonFragment
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if(savedInstanceState == null){
