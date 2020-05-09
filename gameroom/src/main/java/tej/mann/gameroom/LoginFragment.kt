a30e51b7bf9bd6fc071d227591391eed960fd05f
Tej partap Singh <tp3singh@uwaterloo.ca>
2020-05-09 04:12:42 -0400

    minor gradle changes

--- gameroom/src/main/java/tej/mann/gameroom/LoginFragment.kt
+++ gameroom/src/main/java/tej/mann/gameroom/LoginFragment.kt
@@ -64,7 +64,7 @@ class LoginFragment : Fragment() {
 
         viewModel.signInResult().observe(viewLifecycleOwner, Observer {
             if (it != null) {
-                showToast(getString(R.string.welcome, it.displayName))
+                showToast(getString(R.string.welcome, it.email))
                 main_login_button.buttonState = LoginButton.LoginButtonState.DISABLED
                 parentFragmentManager.beginTransaction().replace(R.id.container, RoomFragment())
                     .commit()
